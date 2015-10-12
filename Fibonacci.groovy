019c4e43770f2fc7d9f98e84f49f9e831aa31f20
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-12 14:33:26 +0100

    Recursive method added and tested.

--- Fibonacci.groovy
+++ Fibonacci.groovy
@@ -17,8 +17,17 @@ int fibonacciIterative(int i) {
 }
 
 //Recursive method
+int fibonacciRecursive(int i) {
+	if (i == 1 || i == 2) {
+		return 1;
+	} else {
+		int output = fibonacciRecursive(i-1) + fibonacciRecursive(i-2);
+		return output;
+	}
+}
 
 print "Please insert a positive integer: "
 String s = System.console().readLine();
 int i = Integer.parseInt(s);
-println fibonacciIterative(i)
+println fibonacciIterative(i);
+println fibonacciRecursive(i)
