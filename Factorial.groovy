5e8b85d72cde6a5aeb0a97fe8cda8553d57ca858
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-12 14:00:31 +0100

    First commit. Tested both iterative and recursive methods.

--- Factorial.groovy
+++ Factorial.groovy
@@ -1,18 +1,30 @@
-/**
-	Calculates the factorial of a positive integer.
-	@param i any integer
-	@return the resulting factorial of the integer
-*/
-int factorial(int i) {
-	int result;
-	if (i == 1) {
-		result = 1;
+//Iterative method
+int factorialIterative(int i) {
+	if (i > 0) {
+		int output = 1;
+		for (int count = 1; count <= i; count++) {
+			output = output * count;
+		}
+		return output;
 	} else {
-		result = i * factorial(i-1);
-		return result;
+		return 0;
 	}
 }
 
-// Testing the above method.
-int output = factorial(9);
-println output;
+//Recursive method
+int factorialRecursive(int i) {
+	if (i == 1) {
+		return 1;
+	} else {
+		int output = i * factorialRecursive(i - 1);
+		return output;
+	}
+}
+print "Please insert a positive integer: "
+String s = System.console().readLine();
+int i = Integer.parseInt(s);
+println factorialIterative(i);
+println factorialRecursive(i)
+
+
+
